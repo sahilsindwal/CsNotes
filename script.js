dfa7d2d2045be1714e7fff27a34e092a09eb7633
sahil-sindwal <sahilsindwal@gmail.com>
2022-10-11 20:12:14 -0700

    CsNotes: Cleanup

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-let progress = document.getElementById('progressbar');
-
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function () {
-
-    let progressHeight = (window.pageYOffset /
-        totalHeight) * 100;
-    progress.style.height = progressHeight + "%";
-
-}
-
-
 // this code is used for menubar
 let bar = document.querySelector('#bar');
 let menu = document.querySelector('.java_menu')

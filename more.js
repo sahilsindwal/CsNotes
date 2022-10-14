f65805b43b1436200b46d80859f9ce4f60a1b32f
sahil-sindwal <sahilsindwal@gmail.com>
2022-10-14 01:08:17 -0700

    CsNotes: Add Button to Show More Courses

--- more.js
+++ more.js
@@ -1,14 +1,3 @@
-// this code is used for menubar
-let bar = document.querySelector('#bar');
-let menu = document.querySelector('.java_menu')
-bar.addEventListener('click', () => {
-    bar.classList.toggle("close");
-    menu.classList.toggle("mc");
-
-});
-
-
-
 let arrow = document.querySelector('.arrow');
 let morecourse = document.querySelector('.morecourse');
 arrow.addEventListener('click', () =>{

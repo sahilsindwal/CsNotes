d3f64b6b8788b70d51f2f794b19cc8d1239d11a8
sahil-sindwal <sahilsindwal@gmail.com>
2022-10-14 02:21:37 -0700

    CsNotes: Add Description Section Of Writers

--- js/more.js
+++ js/more.js
@@ -14,3 +14,20 @@ arrow.addEventListener('click', () =>{
       morecourse.style.display = 'block';
     }
   });
+
+
+  // projects work 
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
--- script.js
+++ script.js
@@ -7,21 +7,3 @@ bar.addEventListener('click', () => {
 
 });
 
-
-
-let arrow = document.querySelector('.arrow');
-let morecourse = document.querySelector('.morecourse');
-arrow.addEventListener('click', () =>{
-    arrow.classList.toggle('bx-chevron-right');
-    arrow.classList.toggle('bx-chevron-down');
-    var displaySetting = morecourse.style.display;
-
-  if (displaySetting == 'block') {
-      // clock is visible. hide it
-      morecourse.style.display = 'none';
-    }
-    else {
-      // clock is hidden. show it
-      morecourse.style.display = 'block';
-    }
-  });
